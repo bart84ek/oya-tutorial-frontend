cdf0337b0ab6481345cb292035a0787747c1dfb7
bart <bart.dy@gmail.com>
2019-07-30 12:59:41 +0200

    Values

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const request = require('request');
+const port = process.env.PORT || 3000;
+const api = process.env.API || 'http://localhost:5000';
 const app = express();
-const port = 3000;
-const api = "http://localhost:5000";
 
 app.get('/', (req, res) => {
   request(api + "/", (err, response, body) => {
-    console.log('responseCode:', response && response.statusCode);
     if(response && response.statusCode == 200) {
       body = JSON.parse(body);
-      res.send('<h1>' + body.msg + '</h1>');
+      res.send(`<h1>${body.msg}</h1>`);
     } else {
-      res.send('<h1>Error: Api offline :(</h1><code>' + err + '</code>');
+      res.send(`<h1>Error: Api offline :(</h1><code>${err}</code>`);
     }
   });
 });
