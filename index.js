6d4fa3f2707f3c54b35d64b89e9cd50ad7c6ab1e
bart <bart.dy@gmail.com>
2019-07-30 12:39:32 +0200

    Talk to api

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 3000;
+const api = "http://localhost:5000";
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  request(api + "/", (err, response, body) => {
+    console.log('responseCode:', response && response.statusCode);
+    if(response && response.statusCode == 200) {
+      body = JSON.parse(body);
+      res.send('<h1>' + body.msg + '</h1>');
+    } else {
+      res.send('<h1>Error: Api offline :(</h1><code>' + err + '</code>');
+    }
+  });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`TutFrontend app listening on port ${port}!`))
